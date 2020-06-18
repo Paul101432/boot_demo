27eb025db1e242ccf0b3417d160fe99e91b06f6c
何荣振 <1066783215@qq.com>
2020-06-18 17:26:42 +0800

    task 精简并修改备注

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # boot_demo
 jfinal/springboot 演示
+
+
+## TODO 
++ 基于jboot/springcloud的微服务演示
--- jf/src/main/java/com/sdtech/demo/IndexCtl.java
+++ jf/src/main/java/com/sdtech/demo/IndexCtl.java
@@ -3,7 +3,6 @@ package com.sdtech.demo;
 import com.jfinal.core.Controller;
 
 /**
- * @Description TODO
  * @Date 2020/5/26 11:42
  * @Created by Hrz
  */
